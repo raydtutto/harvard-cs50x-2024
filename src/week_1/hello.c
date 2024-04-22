ff9b897c5725ed1e594edeea578d7cd7cd717b5c
Ann Barilo <raydtutto24@gmail.com>
2024-04-22 12:29:42 +0300

    Add hello.c and CMakeLists.txt

---
+++ src/week_1/hello.c
@@ -0,0 +1,7 @@
+#include <stdio.h> // standard input/output library
+
+int main(void)
+{
+    printf("Hello, world\n"); // 'ptinf' is a function that can output a line of text.
+    // '\n' creates a new line after the words.
+}
