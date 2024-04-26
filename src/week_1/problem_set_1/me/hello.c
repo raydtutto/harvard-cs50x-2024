4e48867ea53f3e6c18910ccec9d4e0f3315a48aa
Ann Barilo <raydtutto24@gmail.com>
2024-04-26 19:39:51 +0300

    Update problem set 1: hello.c

---
+++ src/week_1/problem_set_1/me/hello.c
@@ -0,0 +1,10 @@
+// Says hello to the user
+
+#include <cs50.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    string answer = get_string("What's your name? ");
+    printf("hello, %s\n", answer);
+}
