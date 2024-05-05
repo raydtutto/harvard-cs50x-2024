485beb61cfa9adac411f318c455c86ac471ce2cf
Ann Barilo <raydtutto24@gmail.com>
2024-05-05 20:52:52 +0300

    Add week 3 src, phonebook.c

---
+++ src/week_3_algorithms/notes/src/phonebook.c
@@ -0,0 +1,11 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    string names[] = {"Carter", "David", "John"};
+    string numbers[] = {"617-495-1000", "617-495-1000", "949-468-2750"};
+
+    return 0;
+}
