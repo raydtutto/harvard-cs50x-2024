9dc3547bd0c100505fccf17f4723a7cfe36ec8b0
Ann Barilo <raydtutto24@gmail.com>
2024-05-06 02:01:54 +0300

    Add week 3 src, phonebook_1.c

---
+++ src/week_3_algorithms/notes/src/phonebook_1.c
@@ -0,0 +1,23 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    string names[] = {"Carter", "David", "John"};
+    string numbers[] = {"+1-617-495-1000", "+1-617-495-1000", "+1-949-468-2750"};
+
+    string name = get_string("Name: ");
+
+    for (int i = 0; i < sizeof(names)/sizeof(names[0]); ++i)
+    {
+        if (strcmp(names[i], name) == 0)
+        {
+            printf("Found %s\n", numbers[i]);
+            return 0;
+        }
+    }
+
+    printf("Not found\n");
+    return 1;
+}
