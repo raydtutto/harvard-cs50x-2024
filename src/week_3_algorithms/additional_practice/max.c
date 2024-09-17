918f4eaa3cda62758be715ce22b37864eb7f5808
raydtutto <raydtutto24@gmail.com>
2024-09-17 20:40:12 +0300

    master | add additional_practice max.c

---
+++ src/week_3_algorithms/additional_practice/max.c
@@ -0,0 +1,43 @@
+// Practice writing a function to find a max value
+
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+int max(int array[], int n);
+
+int main(void)
+{
+    int n;
+    do
+    {
+        n = get_int("Number of elements: ");
+    }
+    while (n < 1);
+
+    int arr[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = get_int("Element %i: ", i);
+    }
+
+    printf("The max value is %i.\n", max(arr, n));
+}
+
+// TODO: return the max value
+int max(int array[], int n)
+{
+    int i = 0;
+    int max = array[i];
+
+    for (i = 0; i < n; ++i)
+    {
+        if (max < array[i])
+        {
+            max = array[i];
+        }
+    }
+
+    return max;
+}
