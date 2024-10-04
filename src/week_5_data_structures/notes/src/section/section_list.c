8ed08e1a4c2809f3719ac651f372bb403f72962b
raydtutto <raydtutto24@gmail.com>
2024-10-04 17:11:17 +0300

    master | week 5 | notes | src| add section_list.c

---
+++ src/week_5_data_structures/notes/src/section/section_list.c
@@ -0,0 +1,88 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node
+{
+    string phrase;
+    struct node *next;
+}
+node;
+
+#define LIST_SIZE 2
+
+bool unload(node *list);
+void visualizer(node *list);
+
+int main(void)
+{
+    node *list = NULL;
+
+    // Add items to list
+    for (int i = 0; i < LIST_SIZE; ++i)
+    {
+        string phrase = get_string("Enter a new phrase: ");
+
+        // TODO: add phrase to new node in list.
+        node *n = malloc(sizeof(node));
+        if (n == NULL)
+        {
+            return 2;
+        }
+
+        // Store the phrase from user
+        n->phrase = phrase;
+
+        // Nothing to store in the pointer yet
+        n->next = NULL;
+
+        // Connect in one linked list
+        n->next = list;
+
+        // Reset
+        list = n;
+
+        // Visualize list after adding a node.
+        visualizer(list);
+    }
+
+    // Free all memory used
+    if (!unload(list))
+    {
+        printf("Error freeing the list.\n");
+        return 1;
+    }
+
+    printf("Freed the list.\n");
+    return 0;
+}
+
+bool unload(node *list)
+{
+    // TODO: Free all allocated nodes
+
+    // Temporary pointer to keep track of nodes
+    node *ptr = list;
+
+    while (ptr != NULL)
+    {
+        ptr = list->next;
+        free(list);
+        list = ptr;
+    }
+
+    return true;
+}
+
+void visualizer(node *list)
+{
+    printf("\n+-- List Visualizer --+\n\n");
+    while (list != NULL)
+    {
+        printf("Location: %p\n", list);
+        printf("Phrase: %s\n", list->phrase);
+        printf("Next: %p\n\n", list->next);
+        list = list->next;
+    }
+    printf("\n+---------------------+\n\n");
+}
