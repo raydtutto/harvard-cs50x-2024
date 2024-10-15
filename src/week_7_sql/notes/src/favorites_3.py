30b2ae7a408e5d42a4a868fb9d6451daf68a62aa
raydtutto <raydtutto24@gmail.com>
2024-10-15 20:21:06 +0300

    master | week 7 | notes | add favorites_3.py

---
+++ src/week_7_sql/notes/src/favorites_3.py
@@ -0,0 +1,19 @@
+import csv
+
+# Open the file
+with open("favorites.csv", "r") as file:
+    # Read data and import into reader
+    reader = csv.DictReader(file)
+
+    # Create an empty dictionary
+    counts = {}
+
+    for row in reader:
+        favorite = row["language"]
+        if favorite in counts:
+            counts[favorite] += 1
+        else:
+            counts[favorite] = 1
+
+for favorite in counts:
+    print(f"{favorite}: {counts[favorite]}")
