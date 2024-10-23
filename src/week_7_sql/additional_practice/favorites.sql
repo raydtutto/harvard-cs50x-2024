fd2eca198faa4b5b5b0a1292e45905ff8281ae39
raydtutto <raydtutto24@gmail.com>
2024-10-23 13:15:32 +0300

    master | week 7 | ap | add favorites.sql

---
+++ src/week_7_sql/additional_practice/favorites.sql
@@ -0,0 +1,158 @@
+-- Adventure Time
+-- | 116 | adventure time                     |
+
+UPDATE shows SET title = 'Adventure Time' WHERE title LIKE '%adventure%time%';
+
+-- Arrow
+-- | 24  | arrow                              |
+
+UPDATE shows SET title = 'Arrow' WHERE title LIKE '%arrow%';
+
+-- Avatar: The Last Airbender
+-- | 34  | Avatar The Last Airbender          |
+-- | 29  | Avatar The Last Airbender          |
+-- | 51  | Avatar the Last Airbender          |
+-- | 150 | Avatar the Last Airbender          |
+-- | 36  | Avatar the Last Airbender          |
+-- | 20  | Avatar: The Last Airbender         |
+-- | 101 | Avatar: The Last Airbender         |
+-- | 149 | Avatar: The Last Airbender         |
+
+UPDATE shows SET title = 'Avatar: The Last Airbender' WHERE title LIKE '%avatar%airbender%';
+
+-- Brooklyn Nine-Nine
+-- | 120 | Brooklyn 99                        |
+-- | 28  | Brooklyn-99                        |
+-- | 110 | B99                                |
+-- | 152 | brooklyn 99                        |
+
+UPDATE shows SET title = 'Brooklyn Nine-Nine' WHERE title LIKE '%brooklyn%99%' OR title LIKE '%b%99%';
+
+-- Community
+-- | 72  | community                          |
+-- | 41  | Community                          |
+-- | 74  | Community                          |
+-- | 89  | Community                          |
+-- | 91  | Community                          |
+-- | 96  | Community                          |
+-- | 130 | Community                          |
+-- | 141 | Community                          |
+
+UPDATE shows SET title = 'Community' WHERE title LIKE '%community%';
+
+-- Family Guy
+-- | 4   | Family Guy                         |
+-- | 128 | family guy                         |
+
+UPDATE shows SET title = 'Family Guy' WHERE title LIKE '%family%guy%';
+
+-- Friends
+-- | 80  | friends                            |
+-- | 6   | Friends                            |
+-- | 32  | Friends                            |
+-- | 61  | Friends                            |
+-- | 75  | Friends                            |
+-- | 82  | Friends                            |
+-- | 103 | Friends                            |
+-- | 134 | Friends                            |
+-- | 40  | Friends                            |
+
+UPDATE shows SET title = 'Friends' WHERE title LIKE '%friends%';
+
+-- Game of Thrones
+-- | 126 | Game Of Thrones                    |
+-- | 13  | Game of Thrones                    |
+-- | 52  | Game of Thrones                    |
+-- | 87  | Game of Thrones                    |
+-- | 88  | Game of Thrones                    |
+-- | 139 | Game of Thrones                    |
+-- | 111 | GoT                                |
+
+UPDATE shows SET title = 'Game of Thrones' WHERE title LIKE '%game%of%thrones%' OR title LIKE '%got%';
+
+-- Gilmore Girls
+-- | 78  | Gilmore Girls                      |
+-- | 148 | Gilmore girls                      |
+
+UPDATE shows SET title = 'Gilmore Girls' WHERE title LIKE '%gilmore%girls%';
+
+-- Grey’s Anatomy
+-- | 97  | Grey's Anatomy                     |
+-- | 147 | Grey's anatomy                     |
+
+UPDATE shows SET title = 'Grey’s Anatomy' WHERE title LIKE '%grey%s%natomy%';
+
+-- How I Met Your Mother
+-- | 76  | How I Met Your Mother              |
+-- | 105 | How I Met Your Mother              |
+-- | 1   | How i met your mother              |
+
+UPDATE shows SET title = 'How I Met Your Mother' WHERE title LIKE '%how%i%met%your%mother%';
+
+-- It’s Always Sunny in Philadelphia
+-- | 142 | Its Always Sunny in Philidelphia   |
+
+UPDATE shows SET title = 'It’s Always Sunny in Philadelphia' WHERE title LIKE '%always%sunny%';
+
+-- Parks and Recreation
+-- | 30  | Parks and Rec                      |
+-- | 117 | Parks and Rec                      |
+-- | 59  | Parks and Recreation               |
+
+UPDATE shows SET title = 'Parks and Recreation' WHERE title LIKE '%parks%and%rec%';
+
+-- Sherlock
+-- | 153 | sherlock holmes                    |
+-- | 112 | Sherlock                           |
+-- | 22  | Sherlock Homes                     |
+
+UPDATE shows SET title = 'Sherlock' WHERE title LIKE '%sherlock%';
+
+-- Squid Game
+-- | 39  | Squid Game                         |
+-- | 57  | Squid Game                         |
+-- | 118 | squid game                         |
+
+UPDATE shows SET title = 'Squid Game' WHERE title LIKE '%squid%game%';
+
+-- The Bachelorette
+-- | 107 | the bachelorette                   |
+
+UPDATE shows SET title = 'The Bachelorette' WHERE title LIKE '%bachelorette%';
+
+-- The Crown
+-- | 108 | The CROWN                          |
+-- | 99  | The Crown                          |
+
+UPDATE shows SET title = 'The Crown' WHERE title LIKE '%the%crown%';
+
+-- The Office
+-- | 124 | the office                         |
+-- | 17  | The Office                         |
+-- | 19  | The Office                         |
+-- | 48  | The Office                         |
+-- | 70  | The Office                         |
+-- | 84  | The Office                         |
+-- | 92  | The Office                         |
+-- | 94  | The Office                         |
+-- | 106 | The Office                         |
+-- | 125 | The Office                         |
+-- | 135 | The Office                         |
+-- | 63  | The Office                         |
+-- | 7   | Office                             |
+-- | 10  | Office                             |
+-- | 133 | ThE OffiCE                         |
+
+UPDATE shows SET title = 'The Office' WHERE title LIKE '%office%';
+
+-- The Queen’s Gambit
+-- | 73  | Queen’s Gambit                     |
+
+UPDATE shows SET title = 'The Queen’s Gambit' WHERE title LIKE '%queen%gambit%';
+
+-- The Untamed
+-- | 14  | the Untamed                        |
+
+UPDATE shows SET title = 'The Untamed' WHERE title LIKE '%untamed%';
+UPDATE shows SET title = 'Billions' WHERE title LIKE '%billions%';
+UPDATE shows SET title = 'Criminal Minds' WHERE title LIKE '%criminal%minds%';
