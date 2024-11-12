6ed5296370af728b5491b8b402201b1fd666c619
raydtutto <raydtutto24@gmail.com>
2024-11-12 21:33:03 +0200

    master | week 9 | notes | src | add shows

---
+++ src/week_9_flask/notes/src/shows/shows-3/app.py
@@ -0,0 +1,26 @@
+# Searches for shows using Ajax with JSON
+
+from cs50 import SQL
+# Import jsonify function (turn smth to JSON)
+from flask import Flask, jsonify, render_template, request
+
+app = Flask(__name__)
+
+db = SQL("sqlite:///shows.db")
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+# The same code as it was in "shows-2"
+@app.route("/search")
+def search():
+    q = request.args.get("q")
+    if q:
+        shows = db.execute("SELECT * FROM shows WHERE title LIKE ? LIMIT 50", "%" + q + "%")
+    else:
+        shows = []
+    # BUT
+    # Instead of passing any template, we turn that text to JSON
+    return jsonify(shows)
